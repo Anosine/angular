8a33226352e0bb998ae6256fdcbad04a8f2d2fe9
Edmundas <kasauskas.edmundas@gmail.com>
2024-11-10 16:22:34 +0200

    ugh

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SkaiciuokleComponent } from './skaiciuokle/skaiciuokle.component';
+import { ShopListComponent } from './shop-list/shop-list.component';
+import { CommonModule } from '@angular/common';
+
+
+const routes: Routes = [
+  { path: 'skaiciuokle', component: SkaiciuokleComponent },
+  { path: 'shop-list', component: ShopListComponent }
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkaiciuokleComponent } from "./skaiciuokle/skaiciuokle.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkaiciuokleComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
